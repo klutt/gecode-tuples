c038988c2cf860c9a47c6449d3b23506c83a4414
patwotrik <reg@patrikbroman.se>
2015-11-26 10:10:39 +0100

    Report update

--- src/multisteptest/comparesolutions.py
+++ src/multisteptest/comparesolutions.py
@@ -15,13 +15,23 @@ def getOutput(command, arg):
 
 def main(arg):
     intp = getOutput('int', arg)
+    intoptp = getOutput('intopt', arg)
     exactp = getOutput('exact', arg)
     approxp = getOutput('approx', arg)
     if len(arg) < 6:
         print "Error: compare solutions: wrong number of arguments"
         return 0
-    print "Max solutions: " + str(max(len(intp), len(exactp), len(approxp)))
-    if all(x in exactp for x in approxp) and all(x in intp for x in exactp):
+    print "Solutions: "
+    print "int: " + str(len(intp))
+    print "exact: " + str(len(exactp))
+    print "approx: " +str(len(approxp))
+    print "intopt: " + str(len(intoptp))
+    
+    if not (len(intp) == len(intoptp) == len(approxp) == len(exactp)):
+        print "  FAIL - Different number of solutions"
+        return 0
+    
+    if all(x in exactp for x in approxp) and all(x in intp for x in exactp) and all(x in intp for x in intoptp):
         print "   Ok"
         return 1
     else:
